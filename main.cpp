06bcaf4fdbecc5a083cb0a652fb736e2fc021221
A.Arif Demirel <20181706019@stu.khas.edu.tr>
2023-08-27 22:16:32 +0300

    Screens' Update

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@ void loginScreen() {
     //There are no need for the password in the initial launch.
 }
 
+void adminScreen(){
+    cout << "*****Welcome to admin panel*****" << endl;
+    cout << "1. Add book" << endl;
+    cout << "2. Delete book" << endl;
+    cout << "3. Update book" << endl;
+    cout << "4. Exit" << endl;
+    cout << "********************************" << endl;
+}
+
 void bookScreen(){
     cout << "*****Welcome to book adding system*****" << endl;
 }
@@ -25,6 +34,7 @@ int main() {
     int option;
     int entered_id;
     string book_name;
+    string book_writer;
 
     do{
         showMenu();
@@ -33,20 +43,46 @@ int main() {
 
     switch(option){
         case 1:loginScreen();
-            cout << "Write admin ID: " << endl; // Admin IDs will be given to admins with the launch of app
-            cin >> entered_id;
+                cout << "Write admin ID: " << endl; // Admin IDs will be given to admins with the launch of app
+                cin >> entered_id;
             if (entered_id == 6875){
-                bookScreen();
+                adminScreen();
+                if (option == 1){
+                    bookScreen();
+                    cout << "Please write the name of the book: " << endl;
+                    cin >> book_name;
+                    cout << "Please write the writer of the book: ";
+                    cin >> book_writer;
+                }else if(option == 2){
+                    cout << "Please write the name of the book: " << endl;
+                    cin >> book_name;
+                    cout << "Please write the writer of the book: ";
+                    cin >> book_writer;
+                }else if(option == 3){
+                    cout << "Please write the name of the book: " << endl;
+                    cin >> book_name;
+                    cout << "Please write the writer of the book: ";
+                    cin >> book_writer;
+                }
+                else if(option == 4){
+                    cout << "Thank you for using our system" << endl;
+                }
+            }else{
+                cout << "Wrong ID" << endl;
+            }
+                break;
+        case 2: // Search in Library
                 cout << "Please write the name of the book: " << endl;
                 cin >> book_name;
-                cout << "Please write the writer of the book: "
-                cin
-            }else{}; //Buradan devam
+                cout << "Please write the writer of the book: ";
+                cin >> book_writer;
+                break;
+        case 3: // Exit
+                cout << "Thank you for using our system" << endl;
+                break;
+            }
 
-        }while (case !=3);
-    };
-
-
+        }while (option != 3);
 
     return 0;
-}
+};
